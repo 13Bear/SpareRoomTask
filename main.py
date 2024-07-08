9b8470106f3ae524ab37b9be78d471fd32c69fa6
Teddy Seddon <88630040+13Bear@users.noreply.github.com>
2024-07-08 16:26:57 +0100

    Update main.py
    Added basic functionality.
    Does not account for special prices

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+productDataset = {
+            "A": (50),
+            "B": (30),
+            "C": (20),
+            "D": (15)
+        }
 
+def checkout(formattedList):
+    subTotal = 0
+    for item in desiredOutput:
+        subTotal += productDataset.get(item[0])*item[1]
+    return subTotal
+
+exampleList = [{"code":"A","quantity":3},{"code":"B","quantity":3},{"code":"C","quantity":1},{"code":"D","quantity":2}]
+
+desiredOutput = [(item["code"], item["quantity"]) for item in exampleList]
+
+# Print the output
+print(checkout(desiredOutput))
